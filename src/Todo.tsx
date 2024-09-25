77487c78fb5c85eae71fbd3531c2f09613d2ca5b
Neha Methani <nehamethani54@gmail.com>
2024-09-25 23:38:03 +0530

    Added toggel logic

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,16 +1,29 @@
-import React from "react";
-import { Button } from "react-bootstrap";
 
-const Todo = ({ task }: any) => {
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useState } from "react";
+
+const Todo = ({task}: any) => {
+  const [isStriked, setIsStriked] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsStriked(!isStriked); 
+  };
+
+  const strikethroughStyle = {
+    textDecoration: isStriked ? "line-through" : "none" 
+  }
+
   return (
     <>
-      <div> {task}</div>
+      <div><span style={strikethroughStyle}>{task}</span></div>
 
       <div>
         <Button variant="info" className="m-1 rounded-pill">
           Edit
         </Button>
-        <Button variant="success" className="m-1 rounded-pill">
+        <Button variant="success" className="m-1 rounded-pill" onClick={handleButtonClick}>
           Done
         </Button>
       </div>
